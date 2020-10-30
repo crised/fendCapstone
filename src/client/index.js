2bacf0ce7b25025ef79c54c62f87e2bb5eb5cd9b
Cristian Edwards <cristian.edwards@bairesdev.com>
2020-10-30 09:00:42 -0300

    Added callback to generate Button working

---
+++ src/client/index.js
@@ -0,0 +1,7 @@
+import {setElements} from './js/app.js'
+
+import './styles/style.scss'
+
+setElements(document);
+
+// Not exporting here
--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,5 +1,25 @@
-/* Global Variables */
+let generateButton;
 
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+function setElements(document){
+  console.log('ich bin schon da');
+  console.log(document);
+  generateButton = document.getElementById('generate');
+  generateButton.addEventListener('click', cbGenerateButton);
+}
+
+function cbGenerateButton(event){
+  console.log(event);
+  console.log(generateButton);
+
+}
+
+
+export{
+  setElements
+}
+
+
+
+// let d = new Date();
+// let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
