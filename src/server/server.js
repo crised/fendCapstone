51e4111f714aa4ce95e80c6113c3489161302347
Cristian Edwards <cristian.edwards@bairesdev.com>
2020-10-30 13:32:44 -0300

    server responding to input endpoint, all promise chain ready!

--- src/server/server.js
+++ src/server/server.js
@@ -5,7 +5,6 @@ const bodyParser = require('body-parser')
 const cors = require('cors');
 const assert = require('assert');
 
-
 dotenv.config();
 const app = express()
 app.use(bodyParser.urlencoded({extended: false}));
@@ -18,6 +17,11 @@ app.get('/', function (req, res) {
     res.sendFile(path.resolve('../client/views/index.html'))
 })
 
+app.get('/input', function (req, res) {
+    console.log('hi there');
+    promiseChain("Santiago", 0, "Ford F-150", res);
+})
+
 app.listen(8080, function () {
     // console.log('Listening on port 8080!')
 })
@@ -25,11 +29,11 @@ app.listen(8080, function () {
 const geonames = function (city = 'London') {
     const base_url = 'http://api.geonames.org/searchJSON';
     const url = `${base_url}?q=${city}&maxRows=1&username=crised`;
-    fetch(url)
+    return fetch(url)
         .then(res => res.json())
         .then(json => {
             const {lat, lng, countryName} = json.geonames[0];
-            if (lat && lng && countryName) console.log(lat, lng, countryName);
+            if (lat && lng && countryName) return {lat, lng, countryName};
         });
 }
 
@@ -38,25 +42,40 @@ const weatherbit = function (daysAhead = 0, lat = 51.50853, lng = -0.12574) {
     let base_url = 'https://api.weatherbit.io/v2.0';
     base_url = daysAhead ? `${base_url}/forecast/daily` : `${base_url}/current`;
     const url = `${base_url}?lat=${lat}&lon=${lng}&key=${process.env.API_KEY}`;
-    fetch(url)
+    return fetch(url)
         .then(res => res.json())
         .then(json => {
             const {weather, temp, datetime} = json.data[daysAhead];
-            const desc = `The weather for ${datetime} is ${weather.description} with ${temp} degrees.`
-            console.log(desc);
+            return `The weather for ${datetime} is ${weather.description} with ${temp} degrees.`
         })
 }
 
-const pixabay = function (keywords = 'yellow dog') {
+const pixabay = function (keywords = 'London') {
     const url = `https://pixabay.com/api/?key=${process.env.API_KEY_PIXA}&q=${escape(keywords)}`;
-    fetch(url)
+    return fetch(url)
         .then(res => res.json())
         .then(json => {
-            console.log(json.hits[0].previewURL);
+            return json.hits[0].previewURL;
         });
 }
 
-// geonames();
-// console.log(`Your API key is ${process.env.API_KEY}`);
-// weatherbit(0);
-pixabay();
+const promiseChain = function (city = "Madrid", daysAhead = 0, keywords, res) {
+    const stateObj = {}
+    geonames(city).then((data) => {
+        if (data) {
+            return weatherbit(daysAhead, data.lat, data.lng)
+        }
+    }).then(data => {
+        if (data) {
+            stateObj['weatherString'] = data;
+            return pixabay(keywords);
+        }
+    }).then(data => {
+        if (data) {
+            stateObj['imgURL'] = data;
+            console.log(stateObj);
+            res.send(stateObj);
+        }
+    });
+}
+
