ac485f77cdb92b29d714244c323f6da21e440319
Cristian Edwards <cristian.edwards@bairesdev.com>
2020-10-31 06:21:40 -0300

    Added path dependency

--- src/server/server.js
+++ src/server/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const cors = require('cors');
 const assert = require('assert');
+const path = require('path');
 
 dotenv.config();
 const app = express()
